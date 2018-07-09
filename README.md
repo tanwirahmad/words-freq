ee9200a789b5b366f87d17fc7f21af551d7a9009
Tanwir Ahmad <tahmad@abo.fi>
2018-07-09 19:42:24 +0300

    made pygraphviz dependency optional

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 The __parse_viewer.py__ script requires a corpus file (i.e., collection of all the documents), VOSViewer generated map and network file. The script will generate an image file to visulize the clusters (described in the map and network files) including documents indexes. Further, the script also presents the cluster information in a textual format.
 
-### Requirements
-The script requires `pygraphviz` python library. You can use the following command to install it using `pip`:
+### Optional Requirement
+If you want to generate a graph of clusters, you need to install `pygraphviz` python library. You can use the following command to install it using `pip`:
 
 ```pip install pygraphviz```
+
+Read more about installation at http://pygraphviz.github.io/documentation/latest/install.html
--- parse_vosviewer.py
+++ parse_vosviewer.py
@@ -2,11 +2,10 @@ import sys
 import codecs
 import re
 import csv
-import pygraphviz as pgv    
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from argparse import ArgumentParser
 
-g_colors = [
+G_COLORS = [
     "red",
     "aquamarine4",
     "azure4",
@@ -40,16 +39,20 @@ parser.add_argument("map", help="Path of a VOSViewer map file")
 
 parser.add_argument("network", help="Path of a VOSViewer network file")
 
-parser.add_argument("-i", help="Show only intercluster edges in the graph. Default is False", dest="show_intercluster_edges", action="store_true", default=False)
+parser.add_argument("-g", help="Generate cluster graph. Default is False", 
+                    dest="generate_graph", action="store_true", default=False)
+
+parser.add_argument("-i", help="Show only intercluster edges in the graph. Use with -g option. Default is False", 
+                    dest="show_intercluster_edges", action="store_true", default=False)
 
 args = parser.parse_args()
 
-input_file =  args.corpus # "megamart_dragos_corpus.txt" #sys.argv[1]
-map_file =  args.map #"megamart_dragos_map.txt" #sys.argv[1]
-network_file =  args.network #"megamart_dragos_network.txt" #sys.argv[1]
-show_only_intercluster_edge = args.show_intercluster_edges #True
-A=pgv.AGraph(layout='dot', splines=True, overlap=True,
-                    nodesep=0.6)
+
+input_file =   args.corpus #sys.argv[1]
+map_file =  args.map #sys.argv[1]
+network_file = args.network #sys.argv[1]
+show_only_intercluster_edge = args.show_intercluster_edges 
+generate_graph = args.generate_graph
 color_idx = 0
 
 pat_non_alphabets = re.compile("[^a-zA-Z0-9_\s]")
@@ -63,7 +66,7 @@ def remove_non_alphbets(text):
 cluster_of_keys = {}
 
 # Reading the map file
-docs_map = {}
+docs_map = OrderedDict()
 with open(map_file, 'rb') as csvfile:
     reader = csv.reader(csvfile, dialect='excel-tab')
     skip_header = True
@@ -95,59 +98,87 @@ for n, abs in enumerate(raw_abstracts):
         if re.search(term, ntext):
             docs_map[id]['docs'].append(n)
 
-# Building a Graph structure to represent cluster information collected above
-for id in docs_map:
-    # If we already have formed the cluster for this particular cluster ID
-    cluster_id = docs_map[id]["cluster"]
-    term = docs_map[id]["term"]
-
-    if cluster_id in cluster_of_keys:
-        cluster = cluster_of_keys[cluster_id]
-    else:
-        cluster =  A.add_subgraph([], 
-                    name="cluster_" + cluster_id, 
-                    color=g_colors[color_idx],
-                    label="cluster " + cluster_id)
-        cluster_of_keys[cluster_id] = cluster
-        color_idx += 1
-
+# Writing results to a csv file
+with open('results.csv', 'wb') as csvfile:
+    fields_names = ['cluster', 'term', 'list_of_docs']
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
     
-    term_cluster_name = "cluster_" + term
-    docs_node_name = ', '.join(map(str, docs_map[id]["docs"]))
+    # Writing CSV header
+    writer.writerow(fields_names)
 
-    cluster.add_node(docs_node_name)
-    docs_map[id]["graph_node"] = cluster.add_subgraph([docs_node_name,], name=term_cluster_name, label=term)
-    docs_map[id]["docs_node_name"] = docs_node_name
+    #Writing rows
+    for term in docs_map.values():
+        row = [term['cluster'], term['term'], ', '.join(map(str, term['docs']))]
+        writer.writerow(row)
 
-# Connecting edges among clusters based on the network file
-with open(network_file, 'rb') as csvfile:
-    reader = csv.reader(csvfile, dialect='excel-tab')
-    skip_header = True
-    for row in reader:
-        if skip_header:
-            skip_header= False
-            continue
+print "Exported results to results.csv file"
+
+# If generate_graph option is set 
+if generate_graph:
+
+    try:
+        import pygraphviz as pgv
+    except Exception:
+        print "ERROR: You need to install pygraphivz library in order to use this feature"
+        sys.exit()
+
+    A=pgv.AGraph(layout='dot', splines=True, overlap=True,
+                    nodesep=0.6)
+
+    # Building a Graph structure to represent cluster information collected above
+    for id in docs_map:
+        # If we already have formed the cluster for this particular cluster ID
+        cluster_id = docs_map[id]["cluster"]
+        term = docs_map[id]["term"]
+
+        if cluster_id in cluster_of_keys:
+            cluster = cluster_of_keys[cluster_id]
+        else:
+            cluster =  A.add_subgraph([], 
+                        name="cluster_" + cluster_id, 
+                        color=G_COLORS[color_idx],
+                        label="cluster " + cluster_id)
+            cluster_of_keys[cluster_id] = cluster
+
+            color_idx = (color_idx + 1) % len(G_COLORS)
 
         
-        src_node =  docs_map[row[0]]["docs_node_name"]
-        dst_node =  docs_map[row[1]]["docs_node_name"]
-        weight = row[2]
+        term_cluster_name = "cluster_" + term
+        docs_node_name = ', '.join(map(str, docs_map[id]["docs"]))
 
-        if not show_only_intercluster_edge or (docs_map[row[0]]["cluster"] != docs_map[row[1]]["cluster"]):
-            A.add_edge(src_node, dst_node, penwidth=weight)
+        cluster.add_node(docs_node_name)
+        docs_map[id]["graph_node"] = cluster.add_subgraph([docs_node_name,], name=term_cluster_name, label=term)
+        docs_map[id]["docs_node_name"] = docs_node_name
 
-# Reading information from the Cluster Graph and presenting it in a textual format
-for cluster in A.subgraphs():
-    print cluster.graph_attr["label"]
+    # Connecting edges among clusters based on the network file
+    with open(network_file, 'rb') as csvfile:
+        reader = csv.reader(csvfile, dialect='excel-tab')
+        skip_header = True
+        for row in reader:
+            if skip_header:
+                skip_header= False
+                continue
 
-    terms_in_cluster = [(term.graph_attr["label"], term.nodes()[0], len(term.nodes()[0].split(','))) for term in cluster.subgraphs()]
-    terms_in_cluster.sort(key=lambda tup: tup[2], reverse=True) 
+            
+            src_node =  docs_map[row[0]]["docs_node_name"]
+            dst_node =  docs_map[row[1]]["docs_node_name"]
+            weight = row[2]
 
-    for label, docs, freq in terms_in_cluster:
-        print "\t{}:{}".format(label, freq)
-        print "\tDocs: {}\n".format(docs)
-    #print "\n"
+            if not show_only_intercluster_edge or (docs_map[row[0]]["cluster"] != docs_map[row[1]]["cluster"]):
+                A.add_edge(src_node, dst_node, penwidth=weight)
 
-print "Exporting cluster graph as an image file..."
-#print A.string()
-A.draw('words.png', prog='neato')
+    # Reading information from the Cluster Graph and presenting it in a textual format
+    for cluster in A.subgraphs():
+        print cluster.graph_attr["label"]
+
+        terms_in_cluster = [(term.graph_attr["label"], term.nodes()[0], len(term.nodes()[0].split(','))) for term in cluster.subgraphs()]
+        terms_in_cluster.sort(key=lambda tup: tup[2], reverse=True) 
+
+        for label, docs, freq in terms_in_cluster:
+            print "\t{}:{}".format(label, freq)
+            print "\tDocs: {}\n".format(docs)
+        #print "\n"
+
+    print "Exporting cluster graph as an image file..."
+    #print A.string()
+    A.draw('words.png', prog='neato')
